b0684af98ba959f73227a044834124e86f157f4f
Przemysław Pietras <przemyslawp94@gmail.com>
2019-11-14 13:02:25 +0100

    feat: add option to bootstrap only one tag

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,24 @@
 
 export CONF_DIR="$HOME/.dotfiles"
 
+TAG="${1}"
+
 # shellcheck disable=SC1090
 source "${CONF_DIR}/utils.sh"
 blue "Running bootstrap in $PWD"
+
+if [ "${TAG}" ] && ! [ -d "${CONF_DIR}/${TAG}.d" ]; then
+    red "Configuration ${TAG}.d does not exist"
+    exit 1
+fi
+
+if [ "$TAG" ]; then
+    blue "Executing single configuration: $CONF_DIR/$TAG.d"
+
+    # shellcheck disable=SC1090
+    source "${CONF_DIR}/${TAG}.d/install.sh"
+    exit
+fi
 
 for conf in "$CONF_DIR"/*.d; do
     blue "Executing configuration: $conf"

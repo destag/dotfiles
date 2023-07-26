9399812a2db522aa74cbe4c2cc7e41c522528d58
Przemyslaw Pietras <przemyslawp94@gmail.com>
2023-07-27 01:16:52 +0200

    Add rm! alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -9,6 +9,7 @@ alias :q='exit'
 alias rst='exec $SHELL -l'
 alias ssh='TERM=xterm-color ssh'
 alias vpn='sudo openvpn ~/.vpn/openvpn-config/SyncronVPN-other.ovpn'
+alias rm!='rm -rfv'
 
 # tools
 alias a='ansible'

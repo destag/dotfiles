92ac82019b9f341f1ff6a2c4ca993133efeaec0c
Przemyslaw Pietras <przemyslawp94@gmail.com>
2023-08-01 16:52:34 +0200

    Add git-setup script

---
+++ config/git-setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+current_path="$PWD"
+dev_path="$HOME/dev"
+
+case $1 in
+  "work")
+    echo "Using work config"
+    git config user.email "przemyslaw.pietras@syncron.com"
+    git config user.name "Przemyslaw Pietras"
+    exit
+  ;;
+  "dsg")
+    echo "Using dsg config"
+    git config user.email "przemyslawp94@gmail.com"
+    git config user.name "Przemyslaw Pietras"
+    exit
+  ;;
+  *)
+    echo "No setup specified, using path specific setup"
+  ;;
+esac
+
+case "$current_path" in
+  "$dev_path/syncron"*)
+    echo "Using [work] config"
+    git config user.email "przemyslaw.pietras@syncron.com"
+    git config user.name "Przemyslaw Pietras"
+    ;;
+  "$dev_path/free"*)
+    echo "Using [free] config"
+    git config user.email "przemyslawp94@gmail.com"
+    git config user.name "Przemyslaw Pietras"
+    ;;
+  "$dev_path/dsg"*)
+    echo "Using [dsg] config"
+    git config user.email "przemyslawp94@gmail.com"
+    git config user.name "Przemyslaw Pietras"
+    ;;
+  "$dev_path/other"*)
+    echo "Using [other] config"
+    git config user.email "przemyslawp94@gmail.com"
+    git config user.name "Przemyslaw Pietras"
+    ;;
+  *)
+    echo "Current working directory is NOT in any of the desired paths."
+    exit 1
+    ;;
+esac

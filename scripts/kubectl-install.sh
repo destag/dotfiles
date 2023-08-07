f499e03251e8bb1f55db164d5ac6ed946ea78cbd
Przemyslaw Pietras <przemyslawp94@gmail.com>
2023-08-07 15:31:01 +0200

    Add kubectl

---
+++ scripts/kubectl-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl

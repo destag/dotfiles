93521100ca4c19a8da55f7deb30444ba63bf8074
Przemyslaw Pietras <przemyslawp94@gmail.com>
2023-12-15 12:22:44 +0100

    Add leap.nvim

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,4 +1,4 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
 opt.relativenumber = true
@@ -7,4 +7,19 @@ opt.relativenumber = true
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
+-- })
+
+-- autocmd("User", {
+--   callback = function()
+--     vim.cmd.hi("Cursor", "blend=100")
+--     vim.opt.guicursor:append({ "a:Cursor/lCursor" })
+--   end,
+--   pattern = "LeapEnter",
+-- })
+-- autocmd("User", {
+--   callback = function()
+--     vim.cmd.hi("Cursor", "blend=0")
+--     vim.opt.guicursor:remove({ "a:Cursor/lCursor" })
+--   end,
+--   pattern = "LeapLeave",
 -- })

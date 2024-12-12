5802610b129f6c358587121976e3bb1a3985c1f3
Przemyslaw Pietras <przemyslawp94@gmail.com>
2024-12-12 16:40:32 +0100

    nvim: add neogit

--- nvim/lua/plugins/workflow.lua
+++ nvim/lua/plugins/workflow.lua
@@ -1,10 +1,21 @@
 return {
-  "atiladefreitas/dooing",
-  config = function()
-    require("dooing").setup({
-      keymaps = {
-        toggle_window = "<leader>T",
-      },
-    })
-  end,
+  {
+    "atiladefreitas/dooing",
+    config = function()
+      require("dooing").setup({
+        keymaps = {
+          toggle_window = "<leader>T",
+        },
+      })
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
 }

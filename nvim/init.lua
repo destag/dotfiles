4a2af61581ea472a3f94cdde0209c2c19ff92f08
Przemyslaw Pietras <przemyslawp94@gmail.com>
2025-01-24 17:54:23 +0100

    nvim: add user commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -126,10 +126,36 @@ vim.keymap.set("n", "yss", "<Plug>Yssurround")
 vim.keymap.set("n", "ySs", "<Plug>YSsurround")
 vim.keymap.set("n", "ySS", "<Plug>YSsurround")
 
-vim.filetype.add({
-  extension = {
-    html = "html",
-  },
+vim.api.nvim_create_user_command("CopyRelPath", function()
+  local relative_path = vim.fn.expand("%:p:.")
+  vim.fn.setreg("+", relative_path)
+  print("Copied to clipboard: " .. relative_path)
+end, { desc = "Copy the relative path of the current file to the clipboard" })
+
+vim.api.nvim_create_user_command("RunWorkflow", function(opts)
+  local valid_commands = { apply = true, plan = true, output = true }
+  if not valid_commands[opts.args] then
+    vim.notify("Invalid command. Allowed values are: apply, plan, output", vim.log.levels.ERROR)
+    return
+  end
+
+  local file_path = vim.fn.expand("%:p:.")
+  local dir_path = vim.fn.fnamemodify(file_path, ":h")
+
+  local gh_command =
+    string.format("gh workflow run workflow_dispatch.yaml -f command=%s -f path=%s/", opts.args, dir_path)
+
+  local result = vim.fn.system(gh_command)
+
+  if vim.v.shell_error == 0 then
+    vim.notify("Workflow triggered successfully for path: " .. dir_path, vim.log.levels.INFO)
+  else
+    vim.notify("Failed to trigger workflow: " .. result, vim.log.levels.ERROR)
+  end
+end, {
+  nargs = 1,
+  desc = "Run a GitHub workflow with the current buffer path",
+  complete = function() return { "plan", "apply", "output" } end,
 })
 
 -- add binaries installed by mason.nvim to path

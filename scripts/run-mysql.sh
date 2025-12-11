91ad07809dc5c672a0d5adc9e30c7f30de5b11f9
Przemyslaw Pietras <przemyslawp94@gmail.com>
2025-12-11 16:47:54 +0100

    zsh: move aliases to zshrc

--- scripts/run-mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-  --name mariadb \
-  --restart always \
-  -e MARIADB_ROOT_PASSWORD=secretpassword \
-  -p 3306:3306 \
-  -v /var/lib/mysql:/var/lib/mysql \
-  -d mariadb:11
--- scripts/run-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-  --name postgres \
-  --restart always \
-  -e POSTGRES_PASSWORD=postgres \
-  -p 5432:5432 \
-  -v /var/lib/postgresql/data:/var/lib/postgresql/data \
-  -d postgres:15.3
